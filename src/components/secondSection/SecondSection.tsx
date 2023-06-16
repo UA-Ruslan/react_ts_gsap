b445840b11be96cfdd7424b241c1eb9790531eb1
Ruslan Nikolaiets <mnemonikus69@gmail.com>
2023-06-16 11:48:33 +0300

    gsap drow svg test

--- src/components/secondSection/SecondSection.tsx
+++ src/components/secondSection/SecondSection.tsx
@@ -3,24 +3,49 @@ import style from './secondSection.module.scss'
 import contentComponentStyle from './contentComponent/contentComponent.module.scss'
 import gsap from "gsap-trial";
 import {ScrollTrigger} from "gsap-trial/ScrollTrigger";
+import {DrawSVGPlugin} from 'gsap-trial/DrawSVGPlugin'
 import icon01 from '../../img/secondSection/01.png'
 import icon02 from '../../img/secondSection/02.png'
 import icon03 from '../../img/secondSection/03.png'
 import mainImage01 from '../../img/secondSection/main01.png'
 import mainImage02 from '../../img/secondSection/main02.png'
 import mainImage03 from '../../img/secondSection/main03.png'
-
 import ContentComponent from "./contentComponent/ContentComponent";
 
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
 
 const SecondSection = () => {
 
+
+    const formRef: any = useRef(null);
     const refSecondSection: any = useRef(null)
     const refFirstChild: any = useRef(null)
     const refSecondChild: any = useRef(null)
     const refThirdChild: any = useRef(null)
+
+    useEffect(() => {
+        const svgPath = formRef.current.querySelector('path');
+
+        gsap.set(svgPath, {drawSVG: '50% 50%'});
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: formRef.current,
+                start: 'bottom bottom',
+                end: 'bottom 90%',
+                scrub: 0.5,
+                toggleActions: 'play none none reverse',
+                markers: true
+            },
+        });
+
+        tl.to(svgPath, {drawSVG: '0% 100%', duration: 5});
+
+        return () => {
+            tl.kill(); // Зупиняємо анімацію при розмонтуванні компонента
+        };
+    }, []);
 
     useEffect(() => {
         const ctx = gsap.context(() => {
@@ -84,8 +109,7 @@ const SecondSection = () => {
 
             tl.to(child, {x: "100vw", duration: 5, ease: "none",})
                 .to(child, {x: "100vw", duration: 10, ease: "none"})
-                .to(child, {x: "200vw", duration: 5, ease: "none",})
-
+            // .to(child, {x: "200vw", duration: 5, ease: "none",})
 
 
         }, refSecondSection);
@@ -108,9 +132,12 @@ const SecondSection = () => {
         Let’s start with clothing. A typical mistake hiking beginners make is wearing<br/> jeans and regular clothes,
         which
         will get heavy and chafe wif they get <br/> sweaty or wet.</p>
-    const pThirdChild = <p>To start, print out the hiking guide and map. If it’s raining, throw them in a <br/> Zip-Lock bag.
-        Read over the guide, study the map, and have a good idea <br/> of what to expect. I like to know what my next landmark
-        is as I hike. For <br/> example, I’ll read the guide and know that say, in a mile, I make a right turn <br/> at the
+    const pThirdChild = <p>To start, print out the hiking guide and map. If it’s raining, throw them in a <br/> Zip-Lock
+        bag.
+        Read over the guide, study the map, and have a good idea <br/> of what to expect. I like to know what my next
+        landmark
+        is as I hike. For <br/> example, I’ll read the guide and know that say, in a mile, I make a right turn <br/> at
+        the
         junction..</p>
 
 
@@ -148,6 +175,20 @@ const SecondSection = () => {
                 pContent={pThirdChild}
             />
 
+            <div className={style.formWrapper}>
+                <svg ref={formRef} viewBox="300 5 200 200" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        strokeWidth='1'
+                        stroke='white'
+                        fill='none'
+                        d="M 400 10.812 L 474.939 10.812 C 483.141 10.812 489.79 17.461 489.79 25.663 L 489.79 174.798 C 489.79 183 483.141 189.649 474.939 189.649 L 325.804 189.649 C 317.602 189.649 310.953 183 310.953 174.798 L 310.953 25.663 C 310.953 17.461 317.602 10.812 325.804 10.812 L 325.804 10.812 L 400 10.812"></path>
+                </svg>
+                <form>
+                    <input className={`${style.inputCommon} ${style.email}`} type={"email"}/>
+                    <input className={`${style.inputCommon} ${style.name}`} type={"text"}/>
+                    <button className={style.submitBtn}>SUBMIT</button>
+                </form>
+            </div>
         </div>
     )
 }
